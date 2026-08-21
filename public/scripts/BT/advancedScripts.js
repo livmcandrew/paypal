f69a7cff26724ee83fb4f14e8311aeb096048306
livmcandrew <olivia.mcandrew@icloud.com>
2026-08-21 12:07:13 +0100

    update line items

--- public/scripts/BT/advancedScripts.js
+++ public/scripts/BT/advancedScripts.js
@@ -287,8 +287,8 @@ fetch("/btcheckout")
                                 kind: "debit",
                                 unitTaxAmount: "7.00",
                                 description: "Cashmere Knitted Jumper",
-                                productCode: "LivsTest",
-                                total_amount: setAmount
+                                productCode: "1234",
+                                totalAmount: setAmount
                             }
                         ];          
 
